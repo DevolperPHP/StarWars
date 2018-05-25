8fa1353e7c76ba27faf3c95c7e5bba7884a80121
DevolperPHP <devolperphp@gmail.com>
2018-05-26 00:51:32 +0300

    Update StarWars.php

--- src/JuzeXmod/StarWars.php
+++ src/JuzeXmod/StarWars.php
@@ -33,7 +33,7 @@ use pocketmine\tile\Chest;
 use pocketmine\inventory\ChestInventory;
 use onebone\economyapi\EconomyAPI;
 use pocketmine\event\player\PlayerQuitEvent;
-use JuzeXmod\ResetMap;
+use JuzeXmod\StarWars\ResetMap;
 use pocketmine\entity\Effect;
 
 class StarWars extends PluginBase implements Listener {
